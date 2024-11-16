570b3aeba209c232787a6285dd34c6c52acfa45d
VadZsolt <125548074+VadZsolt@users.noreply.github.com>
2024-11-16 17:54:35 +0200

    post_food modified

--- BackEnd/routes/post_food.py
+++ BackEnd/routes/post_food.py
@@ -1,16 +1,28 @@
 from fastapi import APIRouter
 from database.database import get_db
 from models.models import User, Food as FoodModel
-from schemas.schemas import FoodCreate, FoodResponse
+from schemas.schemas import FoodCreate, FoodResponse, UserFood
 from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException
+from routes.user_post_food import create_user_food
 
 router = APIRouter()
 
-@router.post("/foods/", response_model=FoodResponse)  # Use FoodResponse for response
-def create_food(food: FoodCreate, db: Session = Depends(get_db)):  # Accept FoodCreate as input
-    db_food = FoodModel(**food.dict())  # Create FoodModel instance from FoodCreate
+@router.post("/foods/", response_model=UserFood)
+def create_food(food: FoodCreate, user_id: int, grams: float, db: Session = Depends(get_db)):
+    existing_food = db.query(FoodModel).filter(FoodModel.name.ilike(food.name.lower())).all()
+    # Check if there's any existing food with the same name and others
+    if existing_food:
+        for item in existing_food:
+            if (
+                item.carbs == food.carbs and
+                item.protein == food.protein and
+                item.calories == food.calories and
+                item.fat == food.fat
+            ):
+                return create_user_food(user_id=user_id, food_id=item.food_id, grams=grams, db=db)
+    db_food = FoodModel(**food.dict())
     db.add(db_food)
     db.commit()
     db.refresh(db_food)
-    return db_food
+    return create_user_food(user_id=user_id, food_id=db_food.food_id, grams=grams, db=db)
--- BackEnd/routes/user_post_food.py
+++ BackEnd/routes/user_post_food.py
@@ -4,11 +4,13 @@ from models.models import UserFoodLog
 from schemas.schemas import UserFoodBase, UserFood
 from sqlalchemy.orm import Session
 from fastapi import Depends, HTTPException
+from datetime import datetime
 
 router = APIRouter()
 
 @router.post("/user-foods/", response_model=UserFood)
-def create_user_food(user_food: UserFoodBase, db: Session = Depends(get_db)):
+def create_user_food(user_id=int, food_id=int, grams=float, db: Session = Depends(get_db)):
+    user_food=UserFoodBase(user_id=user_id, food_id=food_id, grams=grams, consumed_at=datetime.utcnow())
     db_user_food = UserFoodLog(**user_food.dict())
     db.add(db_user_food)
     db.commit()
